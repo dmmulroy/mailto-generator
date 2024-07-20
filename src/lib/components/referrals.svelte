565d95b2da692879d38e25da493937980c126cc4
Dillon Mulroy <dillon.mulroy@vercel.com>
2024-07-20 00:55:59 -0400

    get the email to link redirects working lol

--- src/lib/components/referrals.svelte
+++ src/lib/components/referrals.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { format } from "date-fns";
-  import { Name } from "$lib/referral.svelte";
+  import { Name, Referral } from "$lib/referral.svelte";
   import { store } from "$lib/referral-store.svelte";
   import Card from "./card.svelte";
 </script>
@@ -13,47 +13,90 @@
       >
         Referrals
       </h3>
-      <p class="text-sm text-muted-foreground">
-        Referrals
-      </p>
+      <p class="text-sm text-muted-foreground">Referrals</p>
     </div>
     <div class="mt-4 h-full overflow-y-scroll pb-14">
       <div class="grid gap-4">
         {#if store.referrals.length > 0}
           {#each store.referrals as referral}
-            <div class="flex flex-col rounded-lg border bg-card text-card-foreground shadow-sm p-4">
+            <div
+              class="flex flex-col rounded-lg border bg-card text-card-foreground shadow-sm p-4"
+            >
               <div class="flex flex-row justify-between">
                 <div class="flex flex-col justify-between">
-                  <a
-                    class="text-blue-500 hover:underline"
-                    href={referral.mailtoLink} 
+                  <span class="text-lg font-semibold"
+                    >{Name.getFullName(referral.name)}</span
                   >
-
-                    <span class="text-lg font-semibold">{Name.getFullName(referral.name)}</span>
-                  </a>
                 </div>
                 <button
                   class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-destructive text-destructive-foreground hover:bg-destructive/90 h-9 rounded-md px-3"
                   onclick={() => store.deleteReferral(referral.id)}
                 >
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="h-4 w-4"
+                  >
                     <path d="M3 6h18"></path>
                     <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
                     <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
                   </svg>
                 </button>
               </div>
-              <div class="flex flex-row justify-between text-sm text-gray-500 dark:text-gray-400 pt-4">
+              <div
+                class="flex flex-row justify-between text-sm text-gray-500 dark:text-gray-400 pt-4"
+              >
                 <span>Referred by: {Name.getFullName(referral.referrer)}</span>
-                <span>Created  {format(referral.timestamp,"EEE, MMM do yyyy")}</span>
-              </div>
-              <div class="flex flex-row justify-between items-center pt-2 gap-4">
-                <a 
-                  href={referral.mailtoLink} 
-                  class="inline-flex grow items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 rounded-md px-8 "
-                  rel="noopener noreferrer"
+                <span
+                  >Created {format(
+                    referral.timestamp,
+                    "EEE, MMM do yyyy",
+                  )}</span
                 >
-                  Preview Email</a>
+              </div>
+              <div
+                class="flex flex-row justify-between items-center pt-2 gap-4"
+              >
+                <a
+                  href={`mailto:dillon.mulroy@gmail.com?subject=Referral%20Introductions`}
+                  class="inline-flex grow items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 rounded-md px-8"
+                  onclick={() => {
+                    console.log(
+                      `<a href="${referral.mailtoLink}">
+                          Click here to refer ${Name.getFullName(referral.name)}
+                        </a>`,
+                    );
+                    const data = new ClipboardItem({
+                      "text/html": new Blob(
+                        [
+                          `Hi ${Name.getFullName(referral.referrer)}, 
+                             <br/>
+                             <a href=${referral.mailtoLink}>Click here to refer ${Name.getFullName(referral.name)}</a>
+
+                             <br/>
+Best,
+                             <br/>
+Dillon Mulroy
+                              `,
+                        ],
+                        { type: "text/html" },
+                      ),
+                    });
+                    navigator.clipboard
+                      .write([data])
+                      .then(() => console.log("copied"))
+                      .catch((error) => console.error(error));
+                  }}
+                >
+                  Send Email</a
+                >
               </div>
             </div>
           {/each}
@@ -64,6 +107,3 @@
     </div>
   </div>
 </Card>
-
-
-
